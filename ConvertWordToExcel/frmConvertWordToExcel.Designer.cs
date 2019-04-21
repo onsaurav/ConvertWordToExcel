d86ca649077f82cbb41c84dae6d6b4383adecb5c
Kartik Chandra Biswas <onsaurav@hotmail.com>
2019-04-21 20:31:19 +0600

    Initial commit

---
+++ ConvertWordToExcel/frmConvertWordToExcel.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ConvertWordToExcel
+{
+    partial class frmConvertWordToExcel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConvertTo = new System.Windows.Forms.Button();
+            this.btnConvertFrom = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(219, 87);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 69);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(363, 13);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Visible = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(300, 87);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Enabled = false;
+            this.txtFrom.Location = new System.Drawing.Point(97, 18);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(254, 20);
+            this.txtFrom.TabIndex = 4;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Enabled = false;
+            this.txtTo.Location = new System.Drawing.Point(97, 41);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(254, 20);
+            this.txtTo.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Convert From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Convert To";
+            // 
+            // btnConvertTo
+            // 
+            this.btnConvertTo.Location = new System.Drawing.Point(357, 41);
+            this.btnConvertTo.Name = "btnConvertTo";
+            this.btnConvertTo.Size = new System.Drawing.Size(18, 21);
+            this.btnConvertTo.TabIndex = 8;
+            this.btnConvertTo.UseVisualStyleBackColor = true;
+            this.btnConvertTo.Click += new System.EventHandler(this.btnConvertTo_Click);
+            // 
+            // btnConvertFrom
+            // 
+            this.btnConvertFrom.Location = new System.Drawing.Point(357, 18);
+            this.btnConvertFrom.Name = "btnConvertFrom";
+            this.btnConvertFrom.Size = new System.Drawing.Size(18, 21);
+            this.btnConvertFrom.TabIndex = 9;
+            this.btnConvertFrom.UseVisualStyleBackColor = true;
+            this.btnConvertFrom.Click += new System.EventHandler(this.btnConvertFrom_Click);
+            // 
+            // frmConvertWordToExcel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(387, 123);
+            this.Controls.Add(this.btnConvertFrom);
+            this.Controls.Add(this.btnConvertTo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnConvert);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmConvertWordToExcel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Convert Word to Excel";
+            this.Load += new System.EventHandler(this.frmConvertWordToExcel_Load);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmConvertWordToExcel_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConvertTo;
+        private System.Windows.Forms.Button btnConvertFrom;
+    }
+}
+
---
+++ ConvertWordToExcel/frmConvertWordToExcel.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Office.Interop;
+using System.Reflection;
+using Microsoft.Office.Interop.Word;
+using Microsoft.Office.Interop.Excel;
+using Application = Microsoft.Office.Interop.Excel.Application;
+using System.Runtime.InteropServices;
+
+namespace ConvertWordToExcel
+{
+    public partial class frmConvertWordToExcel : Form
+    {
+        static object misValue = Missing.Value;
+
+        public frmConvertWordToExcel()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void frmConvertWordToExcel_FormClosed(object sender, FormClosedEventArgs e)
+        {
+           System.Windows.Forms.Application.Exit();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string ColText = "";
+            string WordText = "";
+            
+            if (txtFrom.Text.Trim() == "")
+            {
+                MessageBox.Show("Sorry! Invalid source (word) file selected.", "File", MessageBoxButtons.OK);
+                return;
+            }
+            if (txtTo.Text.Trim() == "")
+            {
+                MessageBox.Show("Sorry! Invalid destination (excel) file path selected.", "File", MessageBoxButtons.OK);
+                return;
+            }
+
+            Workbook xlWB = new Workbook();
+            
+            Worksheet xlWorkSheet = new Worksheet();
+            Application xlApp = new Microsoft.Office.Interop.Excel.ApplicationClass();
+            
+            xlWB=xlApp.Workbooks.Add(misValue);
+
+            xlWorkSheet.Cells[0, 0] = "FIRST NAME";
+            Microsoft.Office.Interop.Excel.Range range1 = (Microsoft.Office.Interop.Excel.Range)xlWorkSheet.Cells[0,0];
+            range1.EntireRow.Font.Bold = true;
+            range1.EntireColumn.AutoFit();
+
+            xlWorkSheet.Cells[0, 0] = "LAST NAME";
+            Microsoft.Office.Interop.Excel.Range range2 = (Microsoft.Office.Interop.Excel.Range)xlWorkSheet.Cells[0, 1];
+            range2.EntireRow.Font.Bold = true;
+            range2.EntireColumn.AutoFit();
+
+            xlWorkSheet.Cells[0, 0] = "EMAIL";
+            Microsoft.Office.Interop.Excel.Range range3 = (Microsoft.Office.Interop.Excel.Range)xlWorkSheet.Cells[0, 2];
+            range3.EntireRow.Font.Bold = true;
+            range3.EntireColumn.AutoFit();
+
+            xlWorkSheet.Cells[0, 0] = "PHONE";
+            Microsoft.Office.Interop.Excel.Range range4 = (Microsoft.Office.Interop.Excel.Range)xlWorkSheet.Cells[0, 3];
+            range4.EntireRow.Font.Bold = true;
+            range4.EntireColumn.AutoFit();
+
+            xlWorkSheet.Cells[0, 0] = "EXPERIENCE";
+            Microsoft.Office.Interop.Excel.Range range5 = (Microsoft.Office.Interop.Excel.Range)xlWorkSheet.Cells[0, 4];
+            range5.EntireRow.Font.Bold = true;
+            range5.EntireColumn.AutoFit();
+
+            xlWorkSheet.Cells[0, 0] = "CAPITAL";
+            Microsoft.Office.Interop.Excel.Range range6 = (Microsoft.Office.Interop.Excel.Range)xlWorkSheet.Cells[0, 5];
+            range6.EntireRow.Font.Bold = true;
+            range6.EntireColumn.AutoFit();
+
+            bool AddState = false;
+            int j = 0; int r = 0;
+            WordText = AllText(txtFrom.Text.Trim());
+            string[] stringSeparators = new string[] { "\r\n" };
+            string[] Temp = WordText.Split(stringSeparators, StringSplitOptions.None);
+            for (int i = 0; i < Temp.Length - 1; i++)
+            {
+                AddState = false;
+                ColText = "";
+                ColText = Temp[i].ToString();
+
+                if (Temp[i].ToString().IndexOf("FIRST NAME") >= 0)
+                {
+                    j++; r++; AddState = true;
+                    ColText.Replace("FIRST NAME", "");
+                    ColText = ColText.Trim();
+                }
+                else if (Temp[i].ToString().IndexOf("LAST NAME") >= 0)
+                {
+                    r++; AddState = true;
+                    ColText.Replace("LAST NAME", "");
+                    ColText = ColText.Trim();
+                }
+                else if (Temp[i].ToString().IndexOf("EMAIL") >= 0)
+                {
+                    r++; AddState = true;
+                    ColText.Replace("EMAIL", "");
+                    ColText = ColText.Trim();
+                }
+                else if (Temp[i].ToString().IndexOf("PHONE") >= 0)
+                {
+                    r++; AddState = true;
+                    ColText.Replace("PHONE", "");
+                    ColText = ColText.Trim();
+                }
+                else if (Temp[i].ToString().IndexOf("EXPERIENCE") >= 0)
+                {
+                    r++; AddState = true;
+                    ColText.Replace("EXPERIENCE", "");
+                    ColText = ColText.Trim();
+                }
+                else if (Temp[i].ToString().IndexOf("CAPITAL") >= 0)
+                {
+                    r++; AddState = true;
+                    ColText.Replace("CAPITAL", "");
+                    ColText = ColText.Trim();
+                }
+                if (AddState == true)
+                {
+                    xlWorkSheet.Cells[r, j] = ColText.Trim();
+                    Microsoft.Office.Interop.Excel.Range range7 = (Microsoft.Office.Interop.Excel.Range)xlWorkSheet.Cells[r, j];
+                    range7.EntireRow.Font.Bold = false;
+                    range7.EntireColumn.AutoFit();
+                }
+            }
+
+            xlWB.SaveAs(txtTo.Text.Trim(), XlFileFormat.xlTextWindows , misValue, misValue, misValue, misValue, XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWB.Close(true, misValue, misValue);
+            xlApp.Quit();
+            ReleaseObject(xlWB);
+            ReleaseObject(xlApp);   
+        }
+
+        private void btnConvertFrom_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog(); 
+            dialog.Filter = "MSWord files (*.doc)|*.doc";
+            dialog.InitialDirectory = "C:";
+            dialog.Title = "Select a doc file";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFrom.Text = dialog.FileName;
+            }
+            else
+            {
+                MessageBox.Show("Sorry! No file is seected.", "File", MessageBoxButtons.OK);
+                txtFrom.Text = "";
+            }           
+        }
+
+        private void btnConvertTo_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog oFBD = new FolderBrowserDialog();
+            if (oFBD.ShowDialog() == DialogResult.OK)
+            {
+                txtTo.Text = FileNameTo(oFBD.SelectedPath,1);
+            }
+            else
+            {
+                MessageBox.Show("Sorry! No file psth is seected.", "File", MessageBoxButtons.OK);
+                txtTo.Text = "";
+            }
+        }
+
+        private string FileNameTo(string path, int i)
+        {
+            string GetExcelName = "";
+            string FName = "Converted_Excel_" + i.ToString("00000")+ ".xls";
+            if (path.Substring(path.Length - 1, 1) == "\\") path = path.Substring(0, path.Length - 1);
+            FName = path + "\\" + FName;
+            if (File.Exists(FName))
+            {
+                i++;
+                GetExcelName = FileNameTo(path, i++);
+            }
+            else
+            {
+                GetExcelName = FName;
+            }
+            return GetExcelName;
+        }
+
+        private void frmConvertWordToExcel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private string AllText(string path)
+        {
+            string returnText = "";
+            try
+            {
+                Microsoft.Office.Interop.Word.ApplicationClass wordApp = new Microsoft.Office.Interop.Word.ApplicationClass();
+                object file = path;
+                object nullobj = System.Reflection.Missing.Value;
+                Microsoft.Office.Interop.Word.Document doc = wordApp.Documents.Open(ref file, ref nullobj, ref nullobj,
+                                                                                    ref nullobj, ref nullobj, ref nullobj,
+                                                                                    ref nullobj, ref nullobj, ref nullobj,
+                                                                                    ref nullobj, ref nullobj, ref nullobj,
+                                                                                    ref nullobj, ref nullobj, ref nullobj, ref nullobj);
+                doc.ActiveWindow.Selection.WholeStory();
+                doc.ActiveWindow.Selection.Copy();
+                IDataObject data = Clipboard.GetDataObject();
+                string allText = data.GetData(DataFormats.Text).ToString();
+                doc.Close(ref nullobj, ref nullobj, ref nullobj);
+                wordApp.Quit(ref nullobj, ref nullobj, ref nullobj);
+                returnText = allText;
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString(), "Read Doc", MessageBoxButtons.OK); }
+            return returnText;
+        }
+
+        private static void ReleaseObject(object obj)
+        {
+            try
+            {
+                Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
